eef719df49b916d385e2be5808b3feb01eabb3f4
Darren Kennedy <kennedy.dmb@gmail.com>
2020-07-28 11:27:44 +0100

    Set up Layout component. Set up Auxillary hoc. Set font family to Open Sans

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Auxillary from '../../hoc/Auxillary';
+
+const layout = (props) => (
+    <Auxillary>
+        <div>Toolbar, Sidedrawer, Backdrop</div>
+        <main>
+            {props.children}
+        </main>
+    </Auxillary>
+);
+
+export default layout;
