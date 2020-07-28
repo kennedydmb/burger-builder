038ec57559081a0565834f07e7a800fa2dfa8e66
Darren Kennedy <kennedy.dmb@gmail.com>
2020-07-28 21:05:30 +0100

    Added Layout component. Added CSS module for Layout. Added Burger Builder container.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import Auxillary from '../../hoc/Auxillary';
+import classes from './Layout.module.css'
 
 const layout = (props) => (
     <Auxillary>
         <div>Toolbar, Sidedrawer, Backdrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Auxillary>
