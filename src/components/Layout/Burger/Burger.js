fc9e2bae8cf7f572f917db5ca31b3427cbc4a6fb
Darren Kennedy <kennedy.dmb@gmail.com>
2020-07-30 13:53:39 +0100

    Added ; to some lines of code that were missing. CSS for Burger will now be shown on page.

--- src/components/Layout/Burger/Burger.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const burger = (props) => {
-    return();
-}
-
-export default burger;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import Auxillary from '../../hoc/Auxillary';
-import classes from './Layout.module.css'
+import classes from './Layout.module.css';
 
 const layout = (props) => (
     <Auxillary>
-        <div>Toolbar, Sidedrawer, Backdrop</div>
+        <div>Toolbar, Siderawer, Backdrop</div>
         <main className={classes.Content}>
             {props.children}
         </main>
