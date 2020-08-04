a00a34bab6df186a0d77affd35c007554271d49e
Darren Kennedy <kennedy.dmb@gmail.com>
2020-08-04 16:17:43 +0100

    Burger.js now displays ingredents from BurgerBuilder. It transforms the object containing hard coded ingredients, into a useable array from the visual representation of the burger.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,9 +4,18 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = ( props ) => {
+    // keys() ransforms ingredients object from BurgerBuilder.js, to a useable array
+    const transformIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            //Have to contruct new array for each element in the object, i.e cheese: 2. Length is 2
+           return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey +i} type={igKey} />
+           }) 
+        });
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
+            {transformIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
